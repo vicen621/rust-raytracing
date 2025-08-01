a6943e4d7a58ac16d3dc9a79f961f15b4567ba5b
Vicen621 <71749981+vicen621@users.noreply.github.com>
2025-08-01 13:28:16 -0300

    use Option<HitRecord> in Hittable::hit()

--- src/hittable.rs
+++ src/hittable.rs
@@ -4,25 +4,32 @@ use crate::{ray::Ray, vec3::{self, Point3, Vec3}};
 pub struct HitRecord {
     pub point: Point3,
     pub normal: Vec3,
-    pub t: f64,
-    pub front_face: bool,
+    pub t: f64
 }
 
 impl HitRecord {
-    pub fn new() -> Self {
-        Default::default()
+    pub fn new(ray: &Ray, root: f64, outward_normal: Vec3) -> Self {
+        let t = root;
+        let point = ray.at(root);
+        let normal = HitRecord::calculate_normal(ray, outward_normal);
+
+        HitRecord {
+            point,
+            normal,
+            t,
+        }
     }
 
-    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) {
-        self.front_face = vec3::dot(r.direction(), outward_normal) < 0.0;
-        self.normal = if self.front_face {
+    fn calculate_normal(r: &Ray, outward_normal: Vec3) -> Vec3 {
+        let front_face = vec3::dot(r.direction(), outward_normal) < 0.0;
+        if front_face {
             outward_normal
         } else {
             -outward_normal
-        };
+        }
     }
 }
 
 pub trait Hittable {
-    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
 }
